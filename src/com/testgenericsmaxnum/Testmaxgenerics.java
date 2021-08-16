4d05e259d1cc0492549e7e0a8b074c6c1170e48d
Gaurip10 <thegauriparhad@gmail.com>
2021-08-16 17:32:07 +0530

    Added: Calculating maximum of three float

--- src/com/testgenericsmaxnum/Testmaxgenerics.java
+++ src/com/testgenericsmaxnum/Testmaxgenerics.java
@@ -1,30 +1,37 @@
 package com.testgenericsmaxnum;
 
 public class Testmaxgenerics {
-    public static void toPrint(Integer[] inputArray) {
-        for (int element : inputArray) {
+    public static void toPrint(Double[] doubleArray) {
+        for (int element : doubleArray) {
             System.out.println(element);
         }
         System.out.println();
     }
-    public static int compareTo(Integer[] inputArray)
+    public static int compareTo(Double[] doubleArray)
     {
-        int max = inputArray[0];
-        for (int element : inputArray) {
-            if (inputArray[1] > max)
-                max = inputArray[1];
-            if (inputArray[2] > max)
-                max = inputArray[2];
+        int max = doubleArray[0];
+        for (int element : doubleArray) {
+            if (doubleArray[1] > max)
+                max = doubleArray[1];
+            if (doubleArray[2] > max)
+                max = doubleArray[2];
         }
         return max;
     }
     public static void main(String[] args)
-    {
-        Integer[] intArray = {30,50,40};
-        Testmaxgenerics.toPrint(intArray);
-        int maximum = Testmaxgenerics.compareTo(intArray);
-        System.out.println("Maximum integer number is:"+maximum);
+        {
+            Integer[] intArray = {50, 10, 20};
+            Double[] doubleArray = {7.6, 9.6, 8.3};
+
+            Testmaxgenerics.toPrint(intArray);
+            int maximum_int = Testmaxgenerics.compareTo(intArray);
+            System.out.println("Maximum integer number is:" + maximum_int);
+            Testmaxgenerics.toPrint(doubleArray);
+            double maximum_float = Testmaxgenerics.compareTo(doubleArray);
+            System.out.println("Maximum floating number is:" + maximum_float);
+        }
     }
+
 }
 
 
