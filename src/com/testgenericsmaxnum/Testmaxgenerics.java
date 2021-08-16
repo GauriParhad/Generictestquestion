816dcc86830a5842914d5548272553e320347abc
Gaurip10 <thegauriparhad@gmail.com>
2021-08-16 17:12:47 +0530

    Added: Welcome to generic program

---
+++ src/com/testgenericsmaxnum/Testmaxgenerics.java
@@ -0,0 +1,7 @@
+package com.testgenericsmaxnum;
+
+public class Testmaxgenerics {
+    public static void main (String[] args) {
+        System.out.println("Welcome to Generic problem ");
+    }
+}
