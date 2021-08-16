d2c32ac50277940e209e7a0e5e7126f2b8a4b1f6
Gaurip10 <thegauriparhad@gmail.com>
2021-08-16 17:43:16 +0530

    Added: Calculating maximum of three strings

--- src/com/testgenericsmaxnum/Testmaxgenerics.java
+++ src/com/testgenericsmaxnum/Testmaxgenerics.java
@@ -1,35 +1,40 @@
 package com.testgenericsmaxnum;
 
 public class Testmaxgenerics {
-    public static void toPrint(Double[] doubleArray) {
-        for (int element : doubleArray) {
+    public static void toPrint(String[] stringArray) {
+        for (String element : stringArray) {
             System.out.println(element);
         }
         System.out.println();
     }
-    public static int compareTo(Double[] doubleArray)
+
+    public static int compareTo(String[] stringArray)
     {
-        int max = doubleArray[0];
-        for (int element : doubleArray) {
-            if (doubleArray[1] > max)
-                max = doubleArray[1];
-            if (doubleArray[2] > max)
-                max = doubleArray[2];
+        int max = stringArray[0];
+        for (int element : stringArray) {
+            if (stringArray[1] > max)
+                max = stringArray[1];
+            if (stringArray[2] > max)
+                max = stringArray[2];
         }
         return max;
     }
     public static void main(String[] args)
         {
-            Integer[] intArray = {50, 10, 20};
-            Double[] doubleArray = {7.6, 9.6, 8.3};
+            //Integer[] intArray = {50, 10, 20};
+            //Double[] doubleArray = {7.6, 9.6, 8.3};
+            String[] stringArray= {"Apple", "Peace","Banana"}
 
-            Testmaxgenerics.toPrint(intArray);
-            int maximum_int = Testmaxgenerics.compareTo(intArray);
-            System.out.println("Maximum integer number is:" + maximum_int);
-            Testmaxgenerics.toPrint(doubleArray);
-            double maximum_float = Testmaxgenerics.compareTo(doubleArray);
-            System.out.println("Maximum floating number is:" + maximum_float);
-        }
+            //Testmaxgenerics.toPrint(intArray);
+            //int maximum_int = Testmaxgenerics.compareTo(intArray);
+            //System.out.println("Maximum integer number is:" + maximum_int);
+            //Testmaxgenerics.toPrint(doubleArray);
+            //double maximum_float = Testmaxgenerics.compareTo(doubleArray);
+            //System.out.println("Maximum floating number is:" + maximum_float);
+            Testmaxgenerics.toPrint(stringArray);
+            String maximum_string = Testmaxgenerics.compareTo(stringArray);
+            System.out.println("Maximum floating number is:" + maximum_string);
+
     }
 
 }
