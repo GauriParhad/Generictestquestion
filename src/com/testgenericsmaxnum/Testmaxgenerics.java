6816729decb8cd0da7c1d7da898fec0319ed7dd1
Gaurip10 <thegauriparhad@gmail.com>
2021-08-16 17:20:50 +0530

    Added: Calculating maximum of three integers

--- src/com/testgenericsmaxnum/Testmaxgenerics.java
+++ src/com/testgenericsmaxnum/Testmaxgenerics.java
@@ -1,7 +1,30 @@
 package com.testgenericsmaxnum;
 
 public class Testmaxgenerics {
-    public static void main (String[] args) {
-        System.out.println("Welcome to Generic problem ");
+    public static void toPrint(Integer[] inputArray) {
+        for (int element : inputArray) {
+            System.out.println(element);
+        }
+        System.out.println();
+    }
+    public static int compareTo(Integer[] inputArray)
+    {
+        int max = inputArray[0];
+        for (int element : inputArray) {
+            if (inputArray[1] > max)
+                max = inputArray[1];
+            if (inputArray[2] > max)
+                max = inputArray[2];
+        }
+        return max;
+    }
+    public static void main(String[] args)
+    {
+        Integer[] intArray = {30,50,40};
+        Testmaxgenerics.toPrint(intArray);
+        int maximum = Testmaxgenerics.compareTo(intArray);
+        System.out.println("Maximum integer number is:"+maximum);
     }
 }
+
+
